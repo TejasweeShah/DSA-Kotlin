8842ee16445562555e11a9abb8652c9837ffa0d3
Tejaswee Shah <tejaswee.shah@gmail.com>
2023-01-03 16:48:20 -0500

    Create 01-Fanatics&Betting.kt

---
+++ src/main/kotlin/interview_questions/01-Fanatics&Betting.kt
@@ -0,0 +1,42 @@
+package interview_questions
+
+//  Online auction graph display
+//  x axis is time
+//  y axis is price
+//  Title item
+//
+//  Given a two-dimension array graph the price over time
+//
+//     +-----+-----+-----+-----+-----+-----+
+//     +                             *     +
+//     +                                   +
+//     +                       *           +
+//   $ +                                   +
+//     +                                   +
+//     +           *                       +
+//     +     *                             +
+//     +                 *                 +
+//     +-----+-----+-----+-----+-----+-----+
+//                time
+//
+//   max x = 5
+//   max y = 8
+//   data = {(1,2), (2, 3), (3, 1), (4, 6), (5, 8)}
+
+/*fun main() {
+    val x = 5
+    val y = 8
+
+
+    for (j in 0..y) {
+        for (i in 0..x) {
+            if (
+                print("+")
+                repeat (5){
+                    print("-")
+                }
+        }
+        println
+    }
+
+}*/
